094674ae359188ac27938fb475a2854ddd1b5b73
b4b4r07 <b4b4r07@gmail.com>
2017-05-15 01:37:50 +0900

    a

--- a.go
+++ a.go
@@ -10,43 +10,76 @@ import (
 
 var spinners = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 
-const items = 10
+// const items = 10
+
+var repos = []string{
+	"b4b4r07/enhancd",
+	"b4b4r07/gomi",
+	"b4b4r07/dotfiles",
+	"zplug/zplug",
+	"zsh-users/antigen",
+	"fujiwara/nssh",
+	"crowi/crowi",
+	"mattn/memo",
+}
 
 func main() {
+	items := len(repos)
 	fmt.Print(aec.Hide)
 	defer func() {
 		fmt.Print(aec.Show)
 	}()
-	for i := 1; i <= items; i++ {
+
+	for i := 0; i < items; i++ {
 		fmt.Printf("\n")
 	}
-	up := aec.Up(items)
-
+	up := aec.Up(uint(items))
 	cmax := len(spinners)
 	c := 0
-	done := false
 	rand.Seed(time.Now().UnixNano())
 
-	for {
-		if rand.Intn(50) == 0 {
-			done = true
+	task := make(chan string)
+	taskquit := make(chan bool)
+	workerquit := make(chan bool)
+
+	// go func() {
+	// loop:
+	// 	for {
+	// 		select {
+	// 		case <-taskquit:
+	// 			workerquit <- true
+	// 			break loop
+	// 		case job := <-task:
+	// 			_ = job
+	// 			// fmt.Println(job)
+	// 		}
+	// 	}
+	// }()
+
+	go func() {
+		for _, repo := range repos {
+			task <- repo
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 		}
+		taskquit <- true
+	}()
+
+	for {
 		fmt.Print(up)
 		if c >= cmax {
 			c = 0
 		}
-		for i := 1; i <= items; i++ {
+		for _, repo := range repos {
 			fmt.Print(aec.EraseLine(2))
-			if done {
-				fmt.Println("✔", "donwloaded")
+			if false {
+				fmt.Println("✔", "donwloaded", repo)
 			} else {
-				fmt.Println(spinners[c], "donwloading")
+				fmt.Println(spinners[c], "donwloading", repo)
 			}
 		}
 		c++
 		time.Sleep(time.Duration(100) * time.Millisecond)
-		if done {
-			break
-		}
 	}
+
+	<-workerquit
 }
