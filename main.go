b2161687a49a61f947c139086df2a535d3bd728f
b4b4r07 <b4b4r07@gmail.com>
2017-05-15 00:06:18 +0900

    sync

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+const MAX = 3
 
 var repos = []string{
 	"b4b4r07/enhancd",
@@ -13,16 +16,37 @@ var repos = []string{
 	"zplug/zplug",
 	"zsh-users/antigen",
 	"fujiwara/nssh",
-}
-
-func heavyTask(sec int) {
-	fmt.Println(sec)
-	time.Sleep(time.Duration(sec) * time.Second)
+	"crowi/crowi",
+	"mattn/memo",
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	var wg sync.WaitGroup
+
+	q := make(chan string, len(repos))
+	for i := 0; i < MAX; i++ {
+		wg.Add(1)
+		go donwload(&wg, q)
+	}
+
 	for i := 0; i < len(repos); i++ {
-		heavyTask(rand.Intn(5))
+		q <- repos[i]
+	}
+	close(q)
+
+	wg.Wait()
+}
+
+func donwload(wg *sync.WaitGroup, q chan string) {
+	rand.Seed(time.Now().UnixNano())
+	defer wg.Done()
+	for {
+		url, ok := <-q
+		if !ok {
+			return
+		}
+		sec := rand.Intn(5)
+		fmt.Println(sec, url)
+		time.Sleep(time.Duration(sec) * time.Second)
 	}
 }
